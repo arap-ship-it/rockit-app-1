cfc2777b89f675667a4557a3c4e827f82ab382dc
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 22:30:02 +0700

    Update 🏛️home.py

--- views/🏛️home.py
+++ views/🏛️home.py
@@ -3,16 +3,14 @@ import streamlit as st
 c1, c2 = st.columns(2)
 
 with c1 :
-
+    
     st.image("254c7d3e4737995ddd5292a3450f70ca.jpg") 
     st.title("ALL ABOUT TYPE SHI") 
-   
-    
+
     
     st.image("ec074ff3e46ebbd225b54f2e60029990.jpg") 
     st.title("GOOD MUSIC CUZZ") 
  
-    
     
     st.image("cab968147a62fe0cb645b51cd794ca60.jpg")
     st.title("ABSOLUTE CINEMA")
