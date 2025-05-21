1504c61b3db6191f7bb9367947ba90d1ee57a2d7
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 22:45:13 +0700

    Update 🏛️home.py

--- views/🏛️home.py
+++ views/🏛️home.py
@@ -6,12 +6,4 @@ with c1 :
     
     st.image("254c7d3e4737995ddd5292a3450f70ca.jpg") 
     st.title("ALL ABOUT TYPE SHI") 
-    
 
-    
-    st.image("ec074ff3e46ebbd225b54f2e60029990.jpg") 
-    st.title("GOOD MUSIC CUZZ") 
- 
-    
-    st.image("cab968147a62fe0cb645b51cd794ca60.jpg")
-    st.title("ABSOLUTE CINEMA")
