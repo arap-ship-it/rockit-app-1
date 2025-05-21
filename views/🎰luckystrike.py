aa13060a4e9a6e2743ef212051362937fa8e8922
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 22:59:42 +0700

    Update 🎰luckystrike.py

--- views/🎰luckystrike.py
+++ views/🎰luckystrike.py
@@ -1,1 +1,11 @@
+import streamlit as st 
 
+c1, c2 = st.columns(2)
+
+with c1 :
+  angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil") 
