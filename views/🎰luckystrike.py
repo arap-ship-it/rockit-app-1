9760112b8a439b3aa70a21cb1e1d00d542837fb3
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 23:06:49 +0700

    Update 🎰luckystrike.py

--- views/🎰luckystrike.py
+++ views/🎰luckystrike.py
@@ -1,15 +1,9 @@
 import streamlit as st 
 
-c1, c2 = st.columns(2)
+st.title ("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
 
-with c1 : st.title ("Aplikasi Sederhana") 
-          st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
-
-
-
-
-
-          angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+     angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
   st.write(f"{angka} adalah Bilangan Genap")
