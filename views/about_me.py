c97ce4f31f6435540aefaaf1e2cfd7e8894051f3
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 20:52:27 +0700

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,3 +1,17 @@
 import streamlit as st 
 
 st.title("~BARUDAWGH~")
+st.write(
+    "BEJA KA BARUDAWGH ONE KOMANDO"
+)
+st.image("aleta02150.jpg")
+st.balloons() 
+st.badge("NO1")
+import streamlit as st
+st.text("Kelakuan Anak Kucai") 
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+    import streamlit as st
