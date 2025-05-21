844d6696b6aa0ef8e6a9fd8bdda85c86cd571258
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 19:50:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import pandas as pd
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
+pages = [
+    st.Page("page_1.py", home, title="Home", icon="🏠"),
+    st.Page("page_2.py", about, title="About", icon="ℹ️"),
+]
+pg = st.navigation(pages)
+pg.run()
