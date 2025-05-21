48d8c22faf8880a1da3a9756276cb10ac894ae5b
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 19:35:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("page_1.py", label="Page 1", icon="1️⃣")
-st.page_link("page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
+st.page("streamlit_app.py", label="Home", icon="🏠")
+st.page("page_1.py", label="Page 1", icon="1️⃣")
+st.page("page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page("http://www.google.com", label="Google", icon="🌎")
