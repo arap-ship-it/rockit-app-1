01e19c73c98fa9e0fabf26c47cfcfff172e2dcd6
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 20:00:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 your-app/
 ├── streamlit_app.py
 └── pages/
-    ├── 1_Home.py
-    └── 2_About.py
+    ├── 1Home.py
+    └── 2About.py
 pages = [
-    st.Page(home, title="Home", icon="🏠"),
-    st.Page(about, title="About", icon="ℹ️"),
+    st.Page(Home, title="Home", icon="🏠"),
+    st.Page(About, title="About", icon="ℹ️"),
 ]
 pg = st.navigation(pages)
 pg.run()
