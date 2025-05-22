ce9f2580cdef01647b637f7af3bf91cf2511659b
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-22 09:04:06 +0700

    Update 🏛️home.py

--- views/🏛️home.py
+++ views/🏛️home.py
@@ -10,6 +10,12 @@ with c1 :
     - All About Movie 
     - All About Music
     - All About Good Stuff """) 
+import streamlit as st 
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
             
               
 
