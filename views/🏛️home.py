13707094f513c5c00b4a9ba1d1cddf1513eaed0b
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-22 09:05:37 +0700

    Update 🏛️home.py

--- views/🏛️home.py
+++ views/🏛️home.py
@@ -16,6 +16,7 @@ sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+    st.balloons() 
             
               
 
