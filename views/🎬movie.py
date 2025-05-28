0ba58995d536ad8f0bcd6173b096ae12714f326a
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 14:54:20 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -12,8 +12,6 @@ running time 97 MINUTES
 directed by JOHN HUGHES
 produced by NED TANEN JOHN HUGHES""") 
 
-starring
-MOLLY RINGWALD ALLY SHEEDY JUDD NELSON
 st.video("snaptik_7109872323947203846.mp4") 
 
 
