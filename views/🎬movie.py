82385918b35fdd138e2553d92519d1404b4c2e56
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 22:36:43 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -4,10 +4,9 @@ c1, c2 = st.columns(2)
 
 with c1 : st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width=200)
 st.title("THE BREAKFAST CLUB") 
-st.write("""
-running time 97 MINUTES/
-directed by JOHN HUGHES/
-produced by NED TANEN JOHN HUGHES""") 
+st.write("running time 97 MINUTE") 
+st.write("directed by JOHN HUGHES") 
+st.write("produced by NED TANEN JOHN HUGHES") 
 
 st.video("snaptik_7109872323947203846.mp4") 
 
