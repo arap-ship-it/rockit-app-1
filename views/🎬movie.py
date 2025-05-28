61c0882b98ec392cd108c89327007734b7c7c0bd
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 14:11:28 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -5,7 +5,7 @@ c1, c2 = st.columns(2)
 with c1 : 
     st.image("cab968147a62fe0cb645b51cd794ca60.jpg")
     st.title("ABSOLUTE CINEMA")
-
-st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width=200") 
+    
+st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width=200) 
 
 
