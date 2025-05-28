34bcadfe91ade06fc6a37ddeb0c9b4de5a566b35
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 14:07:27 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -6,4 +6,7 @@ with c1 :
     st.image("cab968147a62fe0cb645b51cd794ca60.jpg")
     st.title("ABSOLUTE CINEMA")
 
+import streamlit as st 
+st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width= 200") 
+
 
