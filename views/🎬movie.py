96dc0bcdafe37d983aaf38053cb45e49c6a3e8d8
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 14:35:59 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -7,6 +7,46 @@ with c1 :
     st.title("ABSOLUTE CINEMA")
     
 st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width=200)
+st.write("""Sutradara
+John Hughes
+Produser
+Ned Tanen
+John Hughes
+Ditulis oleh
+John Hughes
+Pemeran
+Emilio Estevez
+Paul Gleason
+Anthony Michael Hall
+Judd Nelson
+Molly Ringwald
+Ally Sheedy
+Penata musik
+Keith Forsey
+Gary Chang
+Sinematografer
+Thomas Del Ruth
+Penyunting
+Dede Allen
+Perusahaan
+produksi
+A&M Films
+Channel Productions
+Distributor
+Universal Pictures
+Tanggal rilis
+15 Februari 1985
+Durasi
+97 menit[1]
+Negara
+Amerika Serikat
+Bahasa
+Inggris
+Anggaran
+$1 juta[2]
+Pendapatan
+kotor
+$51.5 juta[3]""") 
 st.video("snaptik_7109872323947203846.mp4") 
 
 
