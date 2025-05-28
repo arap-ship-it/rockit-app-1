177111fa1dfe4ca6b2d3bb963b5898e29eadc374
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-28 14:58:42 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -4,9 +4,13 @@ c1, c2 = st.columns(2)
 
 with c1 : 
 
-    
+import streamlit as st 
+
 st.image("09c249dc4a6e80bbe85646e4d98188df.jpg", width=200)
+
 st.title("THE BREAKFAST CLUB") 
+
+
 st.write("""
 running time 97 MINUTES
 directed by JOHN HUGHES
