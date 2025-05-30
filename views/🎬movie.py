00e96cb82d5086402ed85ddcb4b5600e9efc8e2a
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-31 00:39:32 +0700

    Update 🎬movie.py

--- views/🎬movie.py
+++ views/🎬movie.py
@@ -10,6 +10,13 @@ st.write("produced by NED TANEN JOHN HUGHES")
 
 st.video("snaptik_7109872323947203846.mp4") 
 
+st.image("933073f8b5f71a6dc69a837fb7cbc60e.jpg", width=200)
+st.title("FALLEN ANGLES") 
+st.write("running time 99 MINUTE") 
+st.write("directed by Wong  Kar-Wai") 
+st.write("produced by Jeffrey  Lau")
+
+
 import streamlit as st 
 st.image("933073f8b5f71a6dc69a837fb7cbc60e.jpg", width=200) 
 st.title("FALLEN ANGELS") 
